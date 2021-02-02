1263ac079fa5e7e4246c5093a017f42df570e44d
Kevin Stradtman <stradtkt22@gmail.com>
2021-02-02 10:30:10 -0500

    Added more files

---
+++ circle.py
@@ -0,0 +1,16 @@
+class Circle:
+    def __init__(self, diameter):
+        self.diameter = diameter
+
+    @property
+    def radius(self):
+        return self.diameter / 2
+
+    @radius.setter
+    def radius(self, radius):
+        self.diameter = radius * 2
+
+
+small = Circle(10)
+print(small.diameter)
+print(small.radius)
